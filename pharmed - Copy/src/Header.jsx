b6298b4a402edb0529c26d27ab7acd735e679a57
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2023-06-14 22:27:49 +0500

    ok

--- pharmed - Copy/src/Header.jsx
+++ pharmed - Copy/src/Header.jsx
@@ -6,6 +6,10 @@ import UserContext from './userContext'
 
 function Header() {
   const { setUser, user } = useContext(UserContext);
+  const {setSign, sign} =  useContext(UserContext);
+  const {setDes, des} =  useContext(UserContext);
+  const {setPro, pro} =  useContext(UserContext);
+  const {setCart, cart} =  useContext(UserContext);
   return (
     <div className='HomeMed_Top'>
         <div className='Top_First'><img src="https://human-healthcare.com/wp-content/uploads/2023/02/HH-logo-Option-2-e1678900350865.png" alt="sdfasd" /></div>
@@ -17,7 +21,7 @@ function Header() {
         {user ? (
           
           <div>  
-              <Link to="/" onClick={()=>(setUser(""))}>
+              <Link to="/" onClick={()=>{setUser(""); setSign(""); setCart([]); setDes({}); setPro([])}}>
                 Logout
               </Link>
               </div>
