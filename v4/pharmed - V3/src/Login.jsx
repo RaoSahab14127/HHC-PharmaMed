f2725d00960608a24c7749c95e1dc80ced4d318f
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2023-06-23 22:01:00 +0500

    set

--- v4/pharmed - V3/src/Login.jsx
+++ v4/pharmed - V3/src/Login.jsx
@@ -52,7 +52,8 @@ function Login() {
         <div className="Login_MainDiv">
         <div  className="Login_Logo"> <img src="https://human-healthcare.com/wp-content/uploads/2023/02/HH-logo-Option-2-e1678900350865.png" alt="LOGO" />
             <div className="LoginQoute">"Your health, our priority"</div>
-            <div className="LoginQoute">Access your Personalised VIP Healthcare Plan</div></div>
+            <div className="LoginQoute">Gate Way to access your Personalised </div>
+            <div className="LoginQoute">VIP Healthcare Plan</div></div>
             <div className="Login_Top">
             
             <form onSubmit={handleFormSubmit}>
--- v4/pharmed - V3/src/labfile.jsx
+++ v4/pharmed - V3/src/labfile.jsx
@@ -1,6 +1,77 @@
 import React from 'react'
 import "./labfile.css"
 function labfile() {
+  
+    let movies = [];
+    let  donefetch =false;
+    const { pro } = useContext(UserContext);
+    const { user } = useContext(UserContext);
+    const { loader , setLoader} = useContext(UserContext);
+    const [data, setData] = useState({});
+    const [data1, setData1] = useState([]);
+    let filteredArray= [];
+    let filteredUser= [];
+    const WooCommerce = new WooCommerceRestApi({
+      url: 'https://genmed.pk/',
+      consumerKey: 'ck_c0cc7b39a7003a840e40eeedfb39ab5b83818436',
+      consumerSecret: 'cs_2c3db277388d9812f7ff7d514fca4392fcf085ae',
+      version: 'wc/v3',
+      queryStringAuth: true,
+    });
+    useEffect(() => {
+      if(pro.length === 0){
+        Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vS2KVLO1ZDlL8ufE7v1YdLnoLXy7tK3XQdES1oQjb1WsoumXq-1TUrirLLFAAtYR3u4NCFkZNhP4-Ov/pub?gid=0&single=true&output=csv", {
+           download: true,
+           header: true,
+           complete: (results) => {
+             setData(results.data);
+           },
+         })}
+      
+    }, []);
+    movies = Array.from(data)
+    filteredUser = movies.filter((item1) => item1.EMRNumber === user);
+    useEffect(() => {
+      
+      
+      fetchData();
+      
+    }, [data]);
+    const fetchData = async () => {
+      let allpro = []
+      let i = 0
+      try {
+        while(i<(filteredUser.length)){
+        const response = await WooCommerce.get(`products/${filteredUser[i].Med}`);
+        if((response.data).length===0){
+         
+          break;
+        }
+        allpro = allpro.concat(response.data)      
+        i++;
+        }
+        if(allpro.length!==0){    setData1(allpro); setLoader(true);}
+       
+      } catch (error) {
+        console.error('Error retrieving data:', error);
+      }
+    };
+    filteredArray = data1;
+    const final_array  =[] 
+    if(pro.length === 0){
+     
+    for(var i = 0; i<filteredArray.length; i++){
+      let hdata = {...filteredArray[i], Amounti : filteredUser[i].Amount, Durationi : filteredUser[i].Duration}
+      final_array.push(hdata)
+    } 
+    if(filteredArray.length!==0){
+    filteredArray = final_array}
+    else{
+      filteredArray = [{id: 1, name: "No Med"}]
+    }}
+    else{
+      filteredArray = pro
+      }  
   return (
     <div>labfile</div>
   )
